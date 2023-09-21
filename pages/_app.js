50b095e0ed9fae53306c497b02fd2a81fda84707
Nishant Desai <nishantdesai1306@gmail.com>
2023-09-21 00:33:42 -0400

    preload first testimonial poster if available

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,19 @@ import Head from "next/head";
 import '../styles/globals.scss';
 
 export default function MyApp({ Component, pageProps }) {
+  const firstTestimonialImage = pageProps?.testimonials?.[0]?.video?.poster;
+
   return (
     <div>
       <Head>
         <title>Testimonial Donut Widget Rendered</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {
+          firstTestimonialImage && (
+            <link rel="preload" as="image" href={firstTestimonialImage} />
+          )
+        }
       </Head>
 
       <Component {...pageProps} />
