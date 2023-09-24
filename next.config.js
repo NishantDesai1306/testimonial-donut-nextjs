f63709a3264fe2280e6e7378c0b9d701f68a1d22
Nishant Desai <nishantdesai1306@gmail.com>
2023-09-23 22:25:27 -0400

    feat: forced preact

--- next.config.js
+++ next.config.js
@@ -8,14 +8,12 @@ module.exports = {
       use: ["@svgr/webpack"]
     });
 
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
+    Object.assign(config.resolve.alias, {
+      "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+    });
 
     return config;
   }
